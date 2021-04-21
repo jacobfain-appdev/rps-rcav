605c0757f719a25cf13080185c40ad8514a2fa60
jacobfain <jfain@chicagobooth.edu>
2021-04-21 14:09:20 +0000

    lots of progress on paper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,40 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def homepage
+    render({:template => "game_templates/rules.html.erb"})    
+  end
+  
+  
+  
   def play_rock
-    #write a ton of code 
+     @comp_move = ["rock","paper","scissors"].sample
+    
+    if @comp_move == "rock" 
+      @outcome = "tie"
+    elsif @comp_move == "paper" 
+      @outcome = "lost"
+    elsif @comp_move == "scissors" 
+      @outcome = "won"
+    end 
+
     render({:template => "game_templates/user_rock.html.erb"})
   end
+
+  def play_paper
+    
+    @comp_move = ["rock","paper","scissors"].sample
+    
+    if @comp_move == "rock" 
+      @outcome = "won"
+    elsif @comp_move == "paper" 
+      @outcome = "tied"
+    elsif @comp_move == "scissors" 
+      @outcome = "lost"
+    end 
+
+
+    render({:template => "game_templates/user_paper.html.erb"})
+  end
+
 end
